8a0f9a3331ed6bfb9a43e7b91af179d9a3046811
Timo Schick <schickt93@gmail.com>
2021-10-22 16:52:10 +0200

    Add a shell script to run experiments

---
+++ scripts/run_experiments.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+GPU=$1 # the GPU to run the experiments on
+TASK=$2 # the task we want to evaluate on
+
+export CUDA_VISIBLE_DEVICES=$GPU
+source /mounts/Users/cisintern/schickt/venvs/up/bin/activate
+
+cd $HOME/up/final_version
+
+python run_experiment.py -n -t "${TASK}" -k 0 --batch_size 4
+python run_experiment.py -n -t "${TASK}" -k 3 -c 0 -p sim --batch_size 4
+python run_experiment.py -n -t "${TASK}" -k 3 -c 0 -p uniform --batch_size 4
+python run_experiment.py -n -t "${TASK}" -k 10 -c 0 -p sim --batch_size 4
+python run_experiment.py -n -t "${TASK}" -k 10 -c 0 -p uniform --batch_size 4
+python run_experiment.py -n -t "${TASK}" -k 50 -c 0 -p sim --batch_size 4
+python run_experiment.py -n -t "${TASK}" -k 50 -c 0 -p uniform --batch_size 4
+python run_experiment.py -n -t "${TASK}" -k 3 -c 0 -p concat --batch_size 4
